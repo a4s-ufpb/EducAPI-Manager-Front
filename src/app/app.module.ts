f79803e8d9d4b5ffb3222e9cad0c2e22b066ae83
alefesoares <alefe.guimaressoares@gmail.com>
2021-05-22 16:54:58 -0300

    git push -u origin fet/dev#7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CadastorDesafiosComponent } from './cadastor-desafios/cadastor-desafios.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastorDesafiosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
