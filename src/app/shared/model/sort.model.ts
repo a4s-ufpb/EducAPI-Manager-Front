f7839f27dcb17e2e9202072fed1d48ae661f16aa
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2021-06-22 13:27:57 -0300

    Adicionando models da  paginação

---
+++ src/app/shared/model/sort.model.ts
@@ -0,0 +1,7 @@
+export class Sort {
+    sorted: boolean | undefined;
+    unsorted: boolean | undefined;
+    empty: boolean | undefined;
+  }
+  export const ASC = 'ASC';
+  export const DEC = 'DESC';
